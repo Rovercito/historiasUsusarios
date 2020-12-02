7a38f8c53f8f40cd941a26e48525e6a88868b74e
Rover Serrano <serranorover436@gmail.com>
2020-12-02 12:40:10 -0400

    En proceso

--- CrearUsuarios.xaml.cs
+++ CrearUsuarios.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +20,56 @@ namespace historiasUsusarios
     /// </summary>
     public partial class CrearUsuarios : Window
     {
+        string path = @"D:\textoProyecto\Usuarios.txt";
         public CrearUsuarios()
         {
             InitializeComponent();
+        }
+
+        private void ButtonRegistrar_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxNombre.Text == "" && TextBoxPrimer.Text == "" && TextBoxSegundo.Text == "" && TextBoxCi.Text == "" && Password.Password == "")
+            {
+                MessageBox.Show("No puede dejar en blanco los espacios");
+            }
+            else
+            {
+                try
+                {
+                    //Vamos a usar la clase StreamWrite para crear el archivo
+                    StreamWriter tuberia;
+                    //Primero debemos de ver si ya existe
+                    if (File.Exists(path))
+                    {
+                        tuberia = File.AppendText(path);
+                        //el archivo existe:
+                        //la estructura del archivo sera:
+                        // nombre/carrera/semestre
+                        tuberia.WriteLine(TextBoxNombre.Text + "/" + TextBoxPrimer.Text + "/" + TextBoxSegundo.Text + "/" + TextBoxCi.Text + "/" + Password.Password);
+                        tuberia.Close();
+                        MessageBox.Show("Nuevo Usuario creado con exito");
+                    }
+
+                    else
+                    {
+                        //no existe, hay que crearlo:
+                        StreamWriter tuberiaCrear = File.CreateText(path);
+                        tuberiaCrear.Close();
+                        MessageBox.Show("Se creo el archivo con exito");
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("error " + ex.Message);
+                }
+            }
+            TextBoxNombre.Clear();
+            TextBoxPrimer.Clear();
+            TextBoxSegundo.Clear();
+            TextBoxCi.Clear();
+            Password.Clear();
         }
     }
 }
